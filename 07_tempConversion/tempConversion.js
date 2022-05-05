f1c8963c66b6855eedc88a9b68fa51dd501f96ab
mattwg <mattwglowacki@gmail.com>
2022-05-05 15:17:38 -0400

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
+const ftoc = function(ftemp) {
+  let ctemp = 0;
 
+  ctemp = (ftemp - 32)*(5/9);
+
+  ctemp = Math.round(ctemp * 10) / 10;
+  return ctemp;
 };
 
-const ctof = function() {
+const ctof = function(ctemp) {
+  let ftemp = 0;
 
+  ftemp = (ctemp * 9/5) + 32;
+
+  ftemp = Math.round(ftemp * 10) / 10;
+  return ftemp;
 };
 
 // Do not edit below this line
