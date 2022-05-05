de97b0c237458ae5cd7019fc06407fa9b6b288d5
mattwg <mattwglowacki@gmail.com>
2022-05-05 14:59:55 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    let sum = 0;
+    let large = n2;
+    let small = n1;
 
+    if (n1 > n2) {
+        large = n1;
+        small = n2;
+    }
+
+    if (typeof(n1) !== 'number' || typeof(n2) !== 'number') {
+        return 'ERROR';
+    } else if (n1 < 0 || n2 < 0) {
+        return 'ERROR';
+    } else {
+        for (i = small; i <= large; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
