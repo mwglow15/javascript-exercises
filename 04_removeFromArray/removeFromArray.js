7df6dbc168fdb41b6870d26b966735d21c331191
mattwg <mattwglowacki@gmail.com>
2022-05-05 14:43:46 -0400

    Complete Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
 
+    let array = arguments[0];
+
+    for (let i=1; i<arguments.length; i++) {
+        if (array.includes(arguments[i])) {
+            let index = array.indexOf(arguments[i])
+            array.splice(index,1);
+        }
+    }
+    
+    console.log(array);
+    return array;
 };
 
 // Do not edit below this line
