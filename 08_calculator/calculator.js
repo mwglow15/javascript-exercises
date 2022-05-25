d7e9d0f5c69ea6cab7b3d204671345c9e9c1f082
mattwg <mattwglowacki@gmail.com>
2022-05-24 23:18:26 -0400

    Complete Exericse

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,58 @@
-const add = function() {
-	
+const add = function(a,b) {
+	const sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	const diff = a - b;
+  return diff;
 };
 
-const sum = function() {
+const sum = function(array) {
 	
+  if (array.length === 0) {
+    return 0;
+  }
+
+  const sum = array.reduce((total, value) => {
+    return total + value;
+  });
+  
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
 
+  if (array.length === 0) {
+    return 0;
+  }
+
+  const prod = array.reduce((total, value) => {
+    return total * value;
+  });
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(base, pow) {
+
+  let result = 1;
+
+  for (i=0; i<pow; i++) {
+    result *= base; 
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+	if (num === 0) {
+    return fact;
+  };
+
+  for (i=num; i>0; i--) {
+    fact *= i;
+  };
+  return fact;
 };
 
 // Do not edit below this line
