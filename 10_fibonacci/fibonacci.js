f95d375a130460d67438d4e3edb6fea2f29b8ff9
mattwg <mattwglowacki@gmail.com>
2022-05-26 23:15:26 -0400

    Complete Exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num <= 0) {
+        return 'OOPS';
+    }
+    let num1 = 0;
+    let num2 = 1;
+    for(i=0;i<num;i++) {
+            if (i === 0) {
+                currentNum = 1;
+                continue;
+            }
+        currentNum = num1 + num2
+        num1 = num2;
+        num2 = currentNum; 
+        console.log(currentNum)
+    };
 
+    return currentNum;
 };
 
 // Do not edit below this line
