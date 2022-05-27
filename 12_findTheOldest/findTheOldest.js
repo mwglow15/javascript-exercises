17a1a51650fb89a77eb943f3669a5e8c576edb6e
mattwg <mattwglowacki@gmail.com>
2022-05-26 23:36:49 -0400

    Complete Exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const ordered = people.sort((a,b) => {
+        if(!a.yearOfDeath) {
+            a.yearOfDeath = (new Date()).getFullYear();
+        } else if (!b.yearOfDeath) {
+            b.yearOfDeath = (new Date()).getFullYear();
+        }
+        
+        return a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? -1 : 1;
+    });
+    console.log(ordered);
+    return(ordered[0]);
 };
 
 // Do not edit below this line
