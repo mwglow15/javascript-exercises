a809c3501b62ab19da4973d6717d79a1e3973c6f
mattwg <mattwglowacki@gmail.com>
2022-05-26 22:25:56 -0400

    Complete Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const newString = string.replace(/[^a-z0-9]/gi,'').toLowerCase();
+    
+    let backwardString = '';
+    const startIndex = newString.length-1
+    // console.log(newString.length)
+    for(i=startIndex;i>=0;i--) {
+        backwardString = backwardString.concat(newString[i]);
+        //console.log(i,' ',newString[i]);
+    }
 
+    // console.log(string);
+    // console.log(newString);
+    // console.log(backwardString);
+    return newString === backwardString;
 };
 
 // Do not edit below this line
